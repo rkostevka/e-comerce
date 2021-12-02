96e7b18a9ba1719d6774f52ed2082a8f6f1fd8c3
ruslana <ruslana.voznyuk@gmail.com>
2021-12-02 10:45:15 +0200

    Home page, main menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from "./components/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      Hello world!
+      <HomePage />
     </div>
   );
 }
