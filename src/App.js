59c555884a099aa7aef8f4d77fccedda21e52890
ruslana <ruslana.voznyuk@gmail.com>
2021-12-02 16:00:29 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+
 import './App.css';
-import HomePage from "./pages/homepage/homepage.component";
+
+import HomePage from './pages/homepage/homepage.component';
+import {Route, Routes} from "react-router-dom";
+
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE </h1>
+    </div>
+);
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+      <Routes>
+          <Route exact path='/' element={<HomePage/>} />
+          <Route path='/hats' element={<HatsPage/>} />
+      </Routes>
   );
 }
 
