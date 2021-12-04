e8ca7e4cef2a0e4eb795fadeda30462192cb4288
ruslana <ruslana.voznyuk@gmail.com>
2021-12-04 12:09:36 +0200

    Sign in page, sign up user

--- src/pages/sign-in-page/sign-in-page.comonent.jsx
+++ src/pages/sign-in-page/sign-in-page.comonent.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './sign-in-page.styles.scss'
 import SignInComponent from "../../components/sign-in/sign-in.component";
+import SignUpComponent from "../../components/sign-up/sign-up.component";
 
 const SignInPage = () => {
     return (
-        <div className='sign-in'>
+        <div className='sign-block'>
             <SignInComponent />
+            <SignUpComponent />
         </div>
     );
 };
