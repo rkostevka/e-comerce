393f6050f747c3ab68d5a1cd532798bc7a7c5253
ruslana <ruslana.voznyuk@gmail.com>
2022-01-06 16:01:53 +0200

    CheckOutPage - added removeItem and add item

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -8,3 +8,13 @@ export const addItem = item => ({
     type: CartTypes.ADD_CART_ITEM,
     payload: item
 })
+
+export const removeItem = item => ({
+    type: CartTypes.REMOVE_CART_ITEM,
+    payload: item
+})
+
+export const deleteItemFromCart = item => ({
+    type: CartTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+})
--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,5 +1,5 @@
+import {addItemToCart, removeItemFromCart} from "./cart-utils";
 import {CartTypes} from "./cart-types";
-import {addItemToCart} from "./cart-utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -13,12 +13,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         case CartTypes.ADD_CART_ITEM:
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
-            }
+            };
+        case CartTypes.REMOVE_CART_ITEM:
+            return {
+                ...state.cartItems,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+        case CartTypes.CLEAR_ITEM_FROM_CART:
+            return  {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            };
         default:
             return state
     }
