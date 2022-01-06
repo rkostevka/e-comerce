bc3a00ba56aeb84349666d62d9cdc725cc7f80e2
ruslana <ruslana.voznyuk@gmail.com>
2022-01-06 16:25:45 +0200

    CheckOutPage - added redux persistor

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import {combineReducers} from "redux";
 import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cart-reducer";
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from "redux-persist";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import logger from 'redux-logger';
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from './root-reducer';
+import {persistStore} from "redux-persist";
 
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware( ...middlewares ));
+export const store = createStore(rootReducer, applyMiddleware( ...middlewares ));
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
